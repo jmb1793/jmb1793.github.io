2e68e7ad23df5c2dbbc9e7dfff1629a707c9882d
Jean-Michael Bernard <jdbernar@ucsd.edu>
2022-10-25 13:45:16 -0400

    more changes 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+React = require('react');
+ReactDOM = require('react-dom/client');
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
