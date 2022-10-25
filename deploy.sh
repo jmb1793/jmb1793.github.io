6d319c83178aadf7b27f0d786329fdcfda39bc74
Jean-Michael Bernard <jdbernar@ucsd.edu>
2022-10-25 14:00:18 -0400

    more changes 2

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:jmb1793/jmb1793.github.io.git main
+git push -f git@github.com:jmb1793/jmb1793.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
